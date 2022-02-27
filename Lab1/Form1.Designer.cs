2b20dceb2b8e9360988357f0335ddb8eb80b514a
Дмитрий Урывчиков <mitya.mur@yandex.ru>
2022-02-27 23:45:46 +0300

    Created

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FactorialProgressBar = new System.Windows.Forms.ProgressBar();
+            this.FactorialTrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FactorialLabel = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FactorialTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FactorialProgressBar
+            // 
+            this.FactorialProgressBar.Location = new System.Drawing.Point(12, 51);
+            this.FactorialProgressBar.Name = "FactorialProgressBar";
+            this.FactorialProgressBar.Size = new System.Drawing.Size(300, 30);
+            this.FactorialProgressBar.TabIndex = 0;
+            // 
+            // FactorialTrackBar
+            // 
+            this.FactorialTrackBar.Location = new System.Drawing.Point(12, 110);
+            this.FactorialTrackBar.Maximum = 20;
+            this.FactorialTrackBar.Minimum = 1;
+            this.FactorialTrackBar.Name = "FactorialTrackBar";
+            this.FactorialTrackBar.Size = new System.Drawing.Size(300, 56);
+            this.FactorialTrackBar.TabIndex = 1;
+            this.FactorialTrackBar.Value = 1;
+            this.FactorialTrackBar.Scroll += new System.EventHandler(this.FactorialTrackBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Прогресс выполнения расчёта :";
+            // 
+            // FactorialLabel
+            // 
+            this.FactorialLabel.AutoSize = true;
+            this.FactorialLabel.Location = new System.Drawing.Point(319, 110);
+            this.FactorialLabel.Name = "FactorialLabel";
+            this.FactorialLabel.Size = new System.Drawing.Size(0, 17);
+            this.FactorialLabel.TabIndex = 3;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(12, 173);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(84, 17);
+            this.ResultLabel.TabIndex = 4;
+            this.ResultLabel.Text = "Результат :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 488);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.FactorialLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FactorialTrackBar);
+            this.Controls.Add(this.FactorialProgressBar);
+            this.Name = "Form1";
+            this.Text = "Factorial";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FactorialTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar FactorialProgressBar;
+        private System.Windows.Forms.TrackBar FactorialTrackBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FactorialLabel;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Numerics;
+
+namespace Lab1
+{
+    public static class Factorial
+    {
+        private static BigInteger N;
+        private static Form1 Form;
+        private static Thread CalcThread = new Thread(Calc);
+
+        public static void StartCalc(Form1 form, int n)
+        {
+            N = (BigInteger) n;
+            Form = form;
+
+            CalcThread = new Thread(Calc);
+            CalcThread.Start();
+        }
+
+        public static void StopCalc()
+        {
+            CalcThread.Abort();
+        }
+
+        private static void Calc()
+        {
+            BigInteger res = 1;
+
+            for (BigInteger i = 2; i <= N; i++) {
+                res *= i;
+                Form1.FactorialCallback(Form, (int) (100 * i / N), 0);
+            }
+
+            Form1.FactorialCallback(Form, 100, res);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FactorialLabel.Text = FactorialTrackBar.Value.ToString();
+        }
+
+
+
+
+        public static void FactorialCallback(Form1 form, int percent, BigInteger res)
+        {
+            if (form.InvokeRequired) {
+                form.Invoke((Action<Form1, int, BigInteger>)FactorialCallback, form, percent, res);
+            }
+            else {
+                form.FactorialProgressBar.Value = percent;
+                if (res != 0) {
+                    form.ResultLabel.Text = /*"Результат : " +*/ res.ToString();
+                }
+            }
+        }
+
+        private void FactorialTrackBar_Scroll(object sender, EventArgs e)
+        {
+            Factorial.StopCalc();
+
+            FactorialLabel.Text = FactorialTrackBar.Value.ToString();
+
+            Factorial.StartCalc(this, FactorialTrackBar.Value);
+        }
+    }
+}
